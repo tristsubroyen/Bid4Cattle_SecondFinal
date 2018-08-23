2ce0eefb6e196034620de698584e7a45387fa648
Sibusiso Mayalo <mylsbu001@myuct.ac.za>
2018-08-23 16:18:10 +0200

    Added packages

--- app/src/main/java/com/example/admin1/bid4cattle_final/BuyCattle.java
+++ app/src/main/java/com/example/admin1/bid4cattle_final/BuyCattle.java
@@ -1,7 +1,12 @@
 package com.example.admin1.bid4cattle_final;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class BuyCattle extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class BuyCattle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        ImageView imageHolder = (ImageView)findViewById(R.id.viewImage);
+        imageHolder.setImageResource(R.drawable.cow1);
+
     }
-}
+
+
+    }
+
--- app/src/main/java/com/example/admin1/bid4cattle_final/ListCattle_Activity.java
+++ app/src/main/java/com/example/admin1/bid4cattle_final/ListCattle_Activity.java
@@ -1,9 +1,12 @@
 package com.example.admin1.bid4cattle_final;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -35,22 +38,52 @@ public class ListCattle_Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Intent intent = getIntent();
+        String fromPrevious = intent.getStringExtra("last");
+        popUp("ola",fromPrevious);
 
-            adapter = new Sell_List( ListCattle_Activity.this, web, imageID);
-            list = (ListView)findViewById(R.id.list);
-            list.setAdapter(adapter);
-            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                    // Toast.makeText(ListCattle_Activity.this, "You clicked at " + web[+position], Toast.LENGTH_SHORT).show();
-                    Intent nextScreen = new Intent(ListCattle_Activity.this, AuctionCattle.class);
-                    nextScreen.putExtra("position",web[+position]);
-                    ListCattle_Activity.this.startActivity(nextScreen);
+        switch (fromPrevious)
+        {
+            case "last":
+                {
+                    populateList(BuyCattle.class);
+                    break;
+
                 }
-            });
+            case "first":
+                {
+                    populateList(AuctionCattle.class);
+                    break;
 
-
+                }
         }
 
+    }
 
+    public void populateList(final Class className) {
+        adapter = new Sell_List(ListCattle_Activity.this, web, imageID);
+        list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Toast.makeText(ListCattle_Activity.this, "You clicked at " + web[+position], Toast.LENGTH_SHORT).show();
+                Intent nextScreen = new Intent(ListCattle_Activity.this, className);
+                nextScreen.putExtra("position", web[+position]);
+                ListCattle_Activity.this.startActivity(nextScreen);
+            }
+        });
+    }
+    private void popUp(String message, String title)
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(ListCattle_Activity.this);
+        alert.setMessage(message).setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        }).setTitle(title).create();
+        alert.show();
+    }
 }
+
